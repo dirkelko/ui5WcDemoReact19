8c4b1c76a337738cc5ea9f2a5da823e0b904cbb7
Dirk Feeken <dirk.feeken@sap.com>
2024-06-26 10:25:38 +0200

    sequence handling

--- src/App.tsx
+++ src/App.tsx
@@ -6,40 +6,59 @@ import "@ui5/webcomponents/dist/TableHeaderCell.js";
 import exercises from "./exercises";
 import "./App.css";
 import { setTheme } from '@ui5/webcomponents-base/dist/config/Theme.js';
-//import "../../../dirkelko/ui5-timer/src/Ui5Timer.js";
-import "ui5-timer/dist/Ui5Timer.js";
-import {useState} from "react";
+import "../../../dirkelko/ui5-timer/src/Ui5Timer.js";
+//import "ui5-timer/dist/Ui5Timer.js";
+import {useState, useRef} from "react";
 
 setTheme('sap_horizon_dark');
 
 //const exercises = async () => Workouts;
 
 function App() {
+  const [exerciseId, setExerciseId] = useState(exercises[0].id);
   const [exercise, setExercise] = useState(exercises[0].name);
   const [duration, setDuration] = useState(exercises[0].duration);
   const [round, setRound] = useState(exercises[0].round);
   const [exRound, setExRound] = useState(exercises[0].ex);
+  const timerRef = useRef(null);
 
 
   function handleRowClick(event: CustomEvent ) {
     console.log(`Row clicked: ${event.detail.row.childNodes[0].innerText} ${event.detail.row.childNodes[1].innerText}`);
     let ex = exercises.find(ex => ex.id === parseInt(event.detail.row.id));
+    setExerciseId(ex!.id);
     setExercise(ex!.name);
     setDuration(ex!.duration);
     setRound(ex!.round);
     setExRound(ex!.ex);
   };
 
+  function handleTimerFinished(event: CustomEvent) {
+    console.log(`Timer finished`);
+    let ex = exercises.find(ex => ex.id === exerciseId+1);
+    console.log(ex);
+    if (ex) {
+      setExerciseId(ex.id);
+      setExercise(ex.name);
+      setDuration(ex.duration);
+      setRound(ex.round);
+      setExRound(ex.ex);
+      timerRef.current.resetTimer();
+    }
+  }
+
   return (
 
     <div>
         <ui5-timer 
         id="myTimer"
+        ref={timerRef}
         title={exercise}
         sub-title={"Exercise " + exRound + "/" + exercises[exercises.length-1].ex}
         sub-sub-title={"Round "+ round + "/" + exercises[exercises.length-1].round}
         duration={duration}
         vbox="0 0 1200 1200"
+        ontimerFinished={handleTimerFinished}
       />
 
       <ui5-table id="table1" overflowMode="Popin" onrowClick={handleRowClick}>
