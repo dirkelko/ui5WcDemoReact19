893a7b10726faf5b6da57268f4fcd4d9b472c56a
Dirk Feeken <dirk.feeken@sap.com>
2024-06-26 08:49:12 +0200

    max width changed

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-# React + TypeScript + Vite
+# UI5 Web Components demo with React 19 RC
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This demo shows an example application using a custom UI5 Web Component (ui5-timer) and the new UI5 Table in UI5 Web Components 2.0.
+It shows a timer above a list or workout exercises. Clicking on a exercise in the list will set the timer which can be started, stopped and reset.
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
+## Dependencies
 
 If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
 
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "@ui5/webcomponents/dist/TableHeaderRow.js";
 import "@ui5/webcomponents/dist/TableHeaderCell.js";
 import exercises from "./exercises";
 import "./App.css";
-//import "../../ui5wc-timer/src/UI5Timer.js";
+//import "../../../dirkelko/ui5-timer/src/Ui5Timer.js";
 import "ui5-timer/dist/Ui5Timer.js";
 import {useState} from "react";
 
@@ -14,23 +14,31 @@ import {useState} from "react";
 function App() {
   const [exercise, setExercise] = useState(exercises[0].name);
   const [duration, setDuration] = useState(exercises[0].duration);
+  const [round, setRound] = useState(exercises[0].round);
+  const [exRound, setExRound] = useState(exercises[0].ex);
+
 
   function handleRowClick(event: CustomEvent ) {
     console.log(`Row clicked: ${event.detail.row.childNodes[0].innerText} ${event.detail.row.childNodes[1].innerText}`);
-    setExercise(event.detail.row.childNodes[1].innerText);
-    setDuration(parseInt(event.detail.row.childNodes[4].innerText));
-  }
+    let ex = exercises.find(ex => ex.id === parseInt(event.detail.row.id));
+    setExercise(ex!.name);
+    setDuration(ex!.duration);
+    setRound(ex!.round);
+    setExRound(ex!.ex);
+  };
 
   return (
+
     <div>
         <ui5-timer 
         id="myTimer"
-        exercise={exercise}
-        exercise-info="DUMDIDELDUM"
+        title={exercise}
+        sub-title={"Exercise " + exRound + "/" + exercises[exercises.length-1].ex}
+        sub-sub-title={"Round "+ round + "/" + exercises[exercises.length-1].round}
         duration={duration}
         vbox="0 0 1200 1200"
       />
- 
+
       <ui5-table id="table1" overflowMode="Popin" onrowClick={handleRowClick}>
         <ui5-table-header-row slot="headerRow">
         <ui5-table-header-cell id="idCol" importance="3" width="50px"><span>Id</span></ui5-table-header-cell>
@@ -40,7 +48,7 @@ function App() {
         <ui5-table-header-cell id="durationCol" min-width="40px" importance="2">Duration</ui5-table-header-cell>
         </ui5-table-header-row>
           {exercises.map((exercise) => (
-            <ui5-table-row key={exercise.id} interactive>
+            <ui5-table-row id={exercise.id} key={exercise.id} interactive>
               <ui5-table-cell><ui5-label><b>{exercise.id}</b></ui5-label></ui5-table-cell>
               <ui5-table-cell><ui5-label><b>{exercise.name}</b></ui5-label></ui5-table-cell>
               <ui5-table-cell><ui5-label>{exercise.description}</ui5-label></ui5-table-cell>
